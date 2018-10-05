d919a76f5f63d8f3e3a7b959faf32a3701a37d82
Mr.stupid bird <1345739670@qq.com>
2018-10-05 15:22:55 +0800

    Project initialization

---
+++ js/main.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+    $('.slickWrapper').slick({
+        autoplay: true,
+        // appendArrows: $('.slickWrapper'),
+        // appendDots: $('.slickWrapper')
+    });
+
+    tabs($(".centerSlider .focusNavigation li"), $('.centerSlider .focusNewsList .con'));  
+    tabs($(".rightColumn .focusNavigation li"), $('.rightColumn .AnnouncementAcademicList .con'));
+
+    $(".js-hover").hover(function(){
+        $(this).children("ul").show();
+    },function() {
+        $(this).children("ul").hide();
+    });
+});
+var tabs = function(tab, con) {
+    con.each(function () {
+        $(this).hasClass('defaultDisplay') ? $(this).show() : $(this).hide()
+    })
+    tab.mouseover(function(){
+        var index = $(this).index();
+        tab.each(function () {
+            $(this).removeClass("current")
+            con.hide();
+        })
+        con.eq(index).show();
+        $(this).addClass("current")
+    });
+}
